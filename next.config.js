10fd38038483e4b08af783c05c65ee73d7e33c36
Jatin <jatin.nagpal222@gmail.com>
2021-08-28 17:32:00 +0530

    Adding back fallback and removing next/image until I can figure out using it or docs improve

--- next.config.js
+++ next.config.js
@@ -4,27 +4,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   ...(process.env.NETLIFY === 'true' && { target: 'serverless' }),
-  images: {
-    deviceSizes: [320, 500, 680, 1040, 2080, 2048, 3120],
-    domains: [
-      'localhost',
-      'images.unsplash.com',
-      'static.gotsby.org',
-      'static.ghost.org',
-      'gatsby.ghost.io',
-      'ghost.org',
-      'repository-images.githubusercontent.com',
-      'www.gravatar.com',
-      'github.githubassets.com',
-      ...(process.env.IMAGE_DOMAINS || '').split(','),
-    ],
-  },
   reactStrictMode: true,
 })
-
-module.exports = {
-  images: {
-    loader: 'imgix',
-    path: 'https://ik.imagekit.io/bazv6s3x6am/blog/',
-  },
-}
